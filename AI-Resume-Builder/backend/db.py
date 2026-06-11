34bdb2bb3e90c42237439c94e006620a92d6c9b6
naveen201402 <naveen2014sai@gmail.com>
2026-06-11 20:40:46 +0530

    Update db.py

--- AI-Resume-Builder/backend/db.py
+++ AI-Resume-Builder/backend/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="ai-resume-builder-naveen2014sai-61d2.c.aivencloud.com",
     user="avnadmin",
-    password="YOUR_AIVEN_PASSWORD",
+    password="AVNS_W-NR1l8wqxcM9mcMzla",
     database="defaultdb",
     port=13287,
     ssl_disabled=False
