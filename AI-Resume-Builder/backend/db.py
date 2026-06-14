8c76a8795df944e218ee68c3185f5bc692b8fbdd
naveen201402 <naveen2014sai@gmail.com>
2026-06-14 15:55:36 +0530

    Update db.py

--- AI-Resume-Builder/backend/db.py
+++ AI-Resume-Builder/backend/db.py
@@ -2,11 +2,11 @@ import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host=os.environ.get("MYSQLHOST", "thomas.proxy.rlwy.net"),
-    user=os.environ.get("MYSQLUSER", "root"),
-    password=os.environ.get("MYSQLPASSWORD", "QSmlQUfJVZlKxllugoWTbHSRdhOlsAye"),
-    database=os.environ.get("MYSQLDATABASE", "railway"),
-    port=int(os.environ.get("MYSQLPORT", 30518))
+    host=os.environ.get("thomas.proxy.rlwy.net"),
+    user=os.environ.get("root"),
+    password=os.environ.get("8ARNMqo7uXgU5NTweEmWn46Hvewjcp1PtqfXTKDZTj29"),
+    database=os.environ.get("railway"),
+    port=int(os.environ.get("58553"))
 )
 
 cursor = db.cursor()
