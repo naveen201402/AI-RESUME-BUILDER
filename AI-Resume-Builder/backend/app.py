243cd920a6f2bde99c8b6ef83d46bf305ddb2ac3
naveen201402 <naveen2014sai@gmail.com>
2026-06-14 16:33:16 +0530

    Update app.py

--- AI-Resume-Builder/backend/app.py
+++ AI-Resume-Builder/backend/app.py
@@ -6,36 +6,18 @@ from routes.resume import resume
 from routes.jobs import jobs
 
 app = Flask(__name__)
+CORS(app)
 
-# Enable CORS
-CORS(
-    app,
-    resources={
-        r"/*": {
-            "origins": "*"
-        }
-    }
-)
-
-# Register Blueprints
 app.register_blueprint(auth)
 app.register_blueprint(resume)
 app.register_blueprint(jobs)
 
-# Home Route
 @app.route("/")
 def home():
-
     return {
         "success": True,
         "message": "AI Resume Builder Backend Running"
     }
 
-# Run Server
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000)
