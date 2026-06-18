4969e3e4c581048facdc984fba7f350b2bdc8b18
naveen201402 <naveen2014sai@gmail.com>
2026-06-18 11:41:04 +0530

    Update db.py

--- AI-Resume-Builder/backend/db.py
+++ AI-Resume-Builder/backend/db.py
@@ -6,7 +6,9 @@ db = mysql.connector.connect(
     user=os.environ.get("MYSQLUSER"),
     password=os.environ.get("MYSQLPASSWORD"),
     database=os.environ.get("MYSQLDATABASE"),
-    port=int(os.environ.get("MYSQLPORT", 3306))
+    port=int(os.environ.get("MYSQLPORT", 3306)),
+     autocommit=True,
+        connection_timeout=20
 )
 
 cursor = db.cursor()
